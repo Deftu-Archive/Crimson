269ed9975489cf598dc15369b0cbfd71ea114722
Wyvest <45589059+Wyvest@users.noreply.github.com>
2021-10-07 17:23:38 +0900

    Javadoc Kotlin DSL

--- core/src/main/kotlin/xyz/qalcyo/requisite/kotlin/dsl/utilities.kt
+++ core/src/main/kotlin/xyz/qalcyo/requisite/kotlin/dsl/utilities.kt
@@ -19,7 +19,6 @@
 package xyz.qalcyo.requisite.kotlin.dsl
 
 import xyz.qalcyo.json.entities.JsonElement
-import xyz.qalcyo.json.kotlin.jsonObject
 import xyz.qalcyo.json.util.JsonHelper
 import xyz.qalcyo.mango.data.Colour
 import xyz.qalcyo.requisite.core.data.ColourRGB
@@ -27,18 +26,52 @@ import java.awt.Color
 import java.util.*
 
 /* Object/Any */
+/**
+ * Provides a "stringified" version of an object.
+ *
+ * @return A "stringified" version of the object.
+ */
 fun Any.stringify(): String = Objects.toString(this)
 
 /* Numbers */
+/**
+ * @return Whether the number is positive.
+ */
 fun Number.isPositive(): Boolean = toInt() >= 0f
+
+/**
+ * @return Whether the number is negative.
+ */
 fun Number.isNegative(): Boolean = toInt() < 0
+
+/**
+ * @return Whether the number is zero.
+ */
 fun Number.isZero(): Boolean = toInt() == 0
+
+/**
+ * @return A percentage value of the number provided between params min and max.
+ */
 fun Number.percentage(min: Int, max: Int): Int = (toInt() - min) / (max - min)
+
+/**
+ * @return Whether the number is between params min and max.
+ */
 fun Number.isBetween(min: Int, max: Int): Boolean = toInt() in (min + 1) until max
 
 /* Colours */
+/**
+ * @return A ColourRGB instance which represents the Color's values.
+ */
 fun Color.toColourRGB(): ColourRGB = ColourRGB(red, green, blue, alpha)
+
+/**
+ * @return A ColourRGB instance which represents the Colour's values.
+ */
 fun Colour.toColourRGB(): ColourRGB = ColourRGB(red, green, blue, alpha)
 
 /* xyz.qalcyo.json.entities.JsonElement */
+/**
+ * @return A prettified String of a JsonElement.
+ */
 fun JsonElement.prettify(): String = JsonHelper.makePretty(this)
